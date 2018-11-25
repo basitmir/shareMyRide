9ff82d3aadf92f000a843f3595762bcdadc1bc59
Basit Mir <iammirbasit@gmail.com>
2018-11-25 10:23:48 +0530

    save the changes

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CREDENTIALS={
-    apiKey: "AIzaSyAqXyjyFKGqYFeKy4T-3v_vA8gj_EWT3EA",
-    authDomain: "ionic-sharemyride.firebaseapp.com",
-    databaseURL: "https://ionic-sharemyride.firebaseio.com",
-    projectId: "ionic-sharemyride",
-    storageBucket: "ionic-sharemyride.appspot.com",
-    messagingSenderId: "990167925848"
+    apiKey: " ",
+    authDomain: " ",
+    databaseURL: " ",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " "
 };
